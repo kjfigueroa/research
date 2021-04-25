8a07163ba1a0262f2e0510a9222539181e8e066e
kevin jessid <kevin.jessid@gmail.com>
2021-04-25 16:00:16 -0500

    7.1 fixing script of cars

--- scripts/cars.php
+++ scripts/cars.php
@@ -1,6 +1,12 @@
-<?php
-
-$carbrand = "chevrolet";
-$carcolor = "bluegray";
-echo "the brand is:" . $carbrand . "and color is:" . $carcolor;
-?>
+<html>
+    <head>
+        <title>Example cars</title>
+    </head>
+    <body>
+        <?php
+        $carbrand = "chevrolet";
+        $carcolor = "bluegray";
+        echo "the brand is: " . $carbrand . "<br>and color is:" . $carcolor;
+        ?>
+    </body>
+</html>

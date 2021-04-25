9b24917fd6a1c69bc06998291bddf427dc29950e
kevin jessid <kevin.jessid@gmail.com>
2021-04-25 18:22:41 -0500

    9.3 fixing css stylesheet and new php page for concat

--- scripts/concat.php
+++ scripts/concat.php
@@ -1,14 +1,20 @@
-<?php
-$fruit1= "orange";
-$fruit2 = "watermellon";
-?>
 <html>
     <head>
         <title>Concat</title>
         <link rel="stylesheet" href="../style.css">
     </head>
     <body>
+        <p>
+            The following code show the below variables:<br>
+            <code>
+                carbrand = chevrolet<br>
+                carcolor = bluegray
+            </code>
+        </p>
+        <hr>
         <?php
+            $fruit1= "orange";
+            $fruit2 = "watermellon";
             echo "the following is the concat of: ";<br>
         ?>
         <code> fruit1 + fruit2 </code><br><hr>

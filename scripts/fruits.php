7c7d0e70e0bbc2b435a2ef9a906ee6e4a962bb23
kevin jessid <kevin.jessid@gmail.com>
2021-04-25 18:34:52 -0500

    10 fixing new php page for concat

---
+++ scripts/fruits.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Concatenating</title>
+        <link rel="stylesheet" href="../style.css">
+    </head>
+    <body>
+        <p>
+            The following code show are concatenating the below variables:<br>
+            <code>
+                fruit1 = orange<br>
+                fruit2 = watermellon
+            </code>
+        </p>
+        <hr>
+        <?php
+            $fruit1 = "orange";
+            $fruit2 = "watermellon";
+            echo "concat of: " . $fruit1 . "and " . $fruit2;
+        ?>
+    </body>
+</html>

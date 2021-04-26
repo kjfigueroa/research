3363236684b8a8db115df0ce19ebad97430e9315
kevin jessid <kevin.jessid@gmail.com>
2021-04-26 10:21:48 -0500

    12.5 fixing the last commit

--- scripts/showName.php
+++ scripts/showName.php
@@ -20,7 +20,7 @@
                 <td>myName.php</td>
                 <td><?php 
                     //calling the myName.php function
-                    include 'functions/myname.php';
+                    include 'functions/myName.php';
                     printname();
                     ?>
                 </td>

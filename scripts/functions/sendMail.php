9361c5616d06a66d2a0112a9bff2f223a8ebf91d
kevin jessid <kevin.jessid@gmail.com>
2021-04-26 17:57:28 -0500

    13 adding send mail function

---
+++ scripts/functions/sendMail.php
@@ -0,0 +1,17 @@
+<?php
+//this function is for send an email through PHP
+function sendmail(){
+    $to = 'kevin.jessid@gmail.com,kevinfm.job@gmail.com,judith2719@hotmail.com';
+    $subject = 'mail from PHP Scripting';
+    $message = 'testing from server Daketa - 192.168.1.112:8082';
+    $headers = 'Content-Type: text/plain; charset=UTF-8';
+    $parameters = '-v';
+    $success = mail($to,$subject,$message,$headers,$parameters);
+    if ($success){
+        $response = 'Message sent successfully.';
+    } else{
+        $response = 'Message could not be sent.';
+    }
+    echo $response;
+    }
+?>
---
+++ scripts/sendingMail.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title>Sending Mail Function</title>
+        <link rel="stylesheet" href="../style.css">
+    </head>
+    <body>
+        <p>
+            The following code just send a mail to:<br>
+            <code>
+            kevin.jessid@gmail.com<br>
+            kevinfm.job@gmail.com<br>
+            judith2719@hotmail.com<br>
+            </code>
+        </p>
+        <hr>
+        <table>
+            <tr>
+                <th>Response</th>
+            </tr>
+            <tr>
+                <td><?php 
+                    //calling the sendMail.php function
+                    include ('functions/sendMail.php');
+                    sendMail();
+                    ?>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>

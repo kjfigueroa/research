70625e3391d019cd1ab0eeddcf1c37501761d5aa
kevin jessid <kevin.jessid@gmail.com>
2021-04-26 18:08:15 -0500

    13.3 fix adding send mail function

--- scripts/functions/sendMail.php
+++ scripts/functions/sendMail.php
@@ -1,9 +1,9 @@
 <?php
 //this function is for send an email through PHP
 function sendmail(){
-    $to = 'kevin.jessid@gmail.com,kevinfm.job@gmail.com,judith2719@hotmail.com';
+    $to = 'kevin.jessid@gmail.com, kevinfm.job@gmail.com';
     $subject = 'mail from PHP Scripting';
-    $message = 'testing from server Daketa - 192.168.1.112:8082';
+    $message = "testing from server Daketa - 192.168.1.112:8082";
     $headers = 'Content-Type: text/plain; charset=UTF-8';
     $parameters = '-v';
     $success = mail($to,$subject,$message,$headers,$parameters);

8dfcb4362ce0878f12a9fa9ccaed608cd3c62382
kevin jessid <kevin.jessid@gmail.com>
2021-04-26 21:16:51 -0500

    13.14 fixing mail sender

--- scripts/functions/sendMail.php
+++ scripts/functions/sendMail.php
@@ -4,10 +4,11 @@
 function sendMail(){
 //    echo 'Subject: Test\n\nValidando Ajustes' | sendmail -v megamankevin7@gmail.com kevin.jessid@gmail.com
 
+    $sender = 'megamankevin7@gmail.com';
     $to = 'kevin.jessid@gmail.com,kevinfm.job@gmail.com,megamankevin7@gmail.com,taniajessid@gmail.com,judith2719@hotmail.com';
     $Subject = 'PHP Scripting';
     $message = "testing from server Daketa - 192.168.1.112:8082";
-    //$headers = 'Content-Type: text/plain; charset=UTF-8';
+    $headers = 'From:' . $sender . 'Content-Type: text/plain; charset=UTF-8';
     $parameters = '-v';
     $success = mail($to,$Subject,$message,$parameters);
     if ($success){
